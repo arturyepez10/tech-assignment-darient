139f580d3a62ca50acd75ab90c5da84e0e1d1bd2
arturyepez10 <arturyepez10@gmail.com>
2021-07-30 17:54:31 -0400

    added the new urls and view functions for creation of objects

--- main/urls.py
+++ main/urls.py
@@ -8,8 +8,11 @@ urlpatterns = [
     path("", views.home, name="Homepage"),
     path("clients", views.clients, name="clients-list"),
     path("clients/<int:id>", views.client, name="client-info"),
+    path("clients/create", views.createClient, name="client-create"),
     path("banks", views.banks, name="banks-list"),
     path("banks/<int:id>", views.bank, name="bank-info"),
+    path("banks/create", views.createBank, name="bank-create"),
     path("credits", views.credits, name="credits-list"),
     path("credits/<int:id>", views.credit, name="credit-info"),
+    path("credits/create", views.createCredit, name="credit-create"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Import the custom models
 from .models import Bank, Client, Credit
+
+# Import the custom Forms
+from .forms import CreateNewBank, CreateNewCredit, CreateNewClient
 
 # Create your views here.
 
@@ -20,6 +23,35 @@ def client(response, id):
 
     return render(response, "main/clients/info.html", { "client": data })
 
+def createClient(response):
+    if response.method == "POST":
+        form = CreateNewClient(response.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            cl = Client(
+                firstName=data["firstName"],
+                lastName=data["lastName"],
+                birthDate=data["birthDate"],
+                age=data["age"],
+                nationality=data["nationality"],
+                address=data["address"],
+                email=data["email"],
+                phone=data["phone"]
+            )
+            cl.save()
+            return HttpResponseRedirect('/clients')
+    else:
+        form = CreateNewClient()
+    
+    variables = {
+        "form": form,
+        "name": "Create Client",
+        "actionName": "/clients/create",
+        "buttonName": "Create New Client"
+    }
+
+    return render(response, "main/create.html", variables) 
+
 # BANKS
 def banks(response):
     data = Bank.objects.all
@@ -31,6 +63,26 @@ def bank(response, id):
 
     return render(response, "main/banks/info.html", { "bank": data })
 
+def createBank(response):
+    if response.method == "POST":
+        form = CreateNewBank(response.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            b = Bank(name=data["name"], address=data["address"], bankType=data["bankType"])
+            b.save()
+            return HttpResponseRedirect('/banks')
+    else: 
+        form = CreateNewBank()
+
+    variables = {
+        "form": form,
+        "name": "Create Bank",
+        "actionName": "/banks/create",
+        "buttonName": "Create New Bank"
+    }
+
+    return render(response, "main/create.html", variables)
+
 # CREDITS
 def credits(response):
     data = Credit.objects.all
@@ -41,3 +93,31 @@ def credit(response, id):
     data = Credit.objects.get(id=id)
 
     return render(response, "main/credits/info.html", { "credit": data })
+
+def createCredit(response):
+    if response.method == "POST":
+        form = CreateNewCredit(response.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            c = Credit(
+                clientOwner=data["clientOwner"],
+                bankOwner=data["bankOwner"],
+                description=data["description"],
+                minPayment=data["minPayment"],
+                maxPayment=data["maxPayment"],
+                termsOfCredits=data["termsOfCredits"],
+                creditType=data["creditType"]
+            )
+            c.save()
+            return HttpResponseRedirect('/credits')
+    else:
+        form = CreateNewCredit()
+
+    variables = {
+        "form": form,
+        "name": "Create Bank",
+        "actionName": "/credits/create",
+        "buttonName": "Assign New Credit"
+    }
+    
+    return render(response, "main/create.html", variables)
