915889190cc66b4a5b88cf7cfc0c66a699d0cf65
arturyepez10 <arturyepez10@gmail.com>
2021-07-30 15:58:57 -0400

    added urls and controllers of the views

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,11 @@ from . import views
 
 # Paths array
 urlpatterns = [
-    
+    path("", views.home, name="Homepage"),
+    path("clients", views.clients, name="clients-list"),
+    path("clients/<int:id>", views.client, name="client-info"),
+    path("banks", views.banks, name="banks-list"),
+    path("banks/<int:id>", views.bank, name="bank-info"),
+    path("credits", views.credits, name="credits-list"),
+    path("credits/<int:id>", views.credit, name="credit-info"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Import the custom models
+from .models import Bank, Client, Credit
+
 # Create your views here.
 
+def home(response):
+    return render(response, "main/home.html", {})
+
+# CLIENTS
+def clients(response):
+    data = Client.objects.all
+
+    return render(response, "main/clients/list.html", { "clients": data })
+
+def client(response, id):
+    data = Client.objects.get(id=id)
+
+    return render(response, "main/clients/info.html", { "client": data })
+
+# BANKS
+def banks(response):
+    data = Bank.objects.all
+
+    return render(response, "main/banks/list.html", { "banks": data })
+
+def bank(response, id):
+    data = Bank.objects.get(id=id)
+
+    return render(response, "main/banks/info.html", { "bank": data })
+
+# CREDITS
+def credits(response):
+    data = Credit.objects.all
+
+    return render(response, "main/credits/list.html", { "credits": data })
+
+def credit(response, id):
+    data = Credit.objects.get(id=id)
+
+    return render(response, "main/credits/info.html", { "credit": data })
